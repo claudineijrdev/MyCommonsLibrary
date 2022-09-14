a7f331d0a778ba8cfc8e119cecf3342247c9dc53
Felipe Vialle <felipe.vialle.ter@arquiteturaestacio.visualstudio.com>
2022-09-14 14:18:28 -0300

    clear index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
 const monitoring = require('./monitoring');
-const express = require('express');
-const { Router } = require('express');
-const { InitializeLoggers, ConsoleLoggerTransport, TraceLog, MessageEnum, ErrorLog, RequestMiddleware,ResponseMiddleware, ErrorMiddleware,httpContext,setContext } = monitoring;
-
-
-const app = express();
-app.use(httpContext)
-app.use(setContext)
-const port = 3000;
-const routes = Router();
-
-const transport = ConsoleLoggerTransport({ name: 'console', level: 'info', space: 1, silent: false });
-InitializeLoggers([transport], 'app');
-
-TraceLog({ message: 'Test Trace Log' }, { messageType: MessageEnum.DB_CONECTED, replaceString: 'Any_DB' });
-ErrorLog(new Error("Test Error Log"),{ trace : 'trace only for test' }, { messageType: MessageEnum.UNCAUGHT_EXCEPTION, replaceString: '' });
-
-app.use(RequestMiddleware)
-app.use(ResponseMiddleware)
-
-routes.get('/', (req, res) => res.send('Hello World!'));
-routes.get('/test', (req, res) => res.send('OK'));
-routes.get('/error', (req, res) => {
-    throw new Error('Test Error on Endpoint')
-});
-
-app.use(routes);
-app.use(ErrorMiddleware)
-
-
-app.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`);
-});
 
 module.exports = {
     monitoring,
 
 }
+// const express = require('express');
+// const { Router } = require('express');
+
+// const app = express();
+// app.use(httpContext)
+// app.use(setContext)
+// const port = 3000;
+// const routes = Router();
+
+// const transport = ConsoleLoggerTransport({ name: 'console', level: 'info', space: 1, silent: false });
+// InitializeLoggers([transport], 'app');
+
+// TraceLog({ message: 'Test Trace Log' }, { messageType: MessageEnum.DB_CONECTED, replaceString: 'Any_DB' });
+// ErrorLog(new Error("Test Error Log"),{ trace : 'trace only for test' }, { messageType: MessageEnum.UNCAUGHT_EXCEPTION, replaceString: '' });
+
+// app.use(RequestMiddleware)
+// app.use(ResponseMiddleware)
+
+// routes.get('/', (req, res) => res.send('Hello World!'));
+// routes.get('/test', (req, res) => res.send('OK'));
+// routes.get('/error', (req, res) => {
+//     throw new Error('Test Error on Endpoint')
+// });
+
+// app.use(routes);
+// app.use(ErrorMiddleware)
+
+
+// app.listen(port, () => {
+//     console.log(`Running at http://localhost:${port}`);
+// });
